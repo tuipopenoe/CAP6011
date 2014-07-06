41a4d3ccb6fae5c09757cea58b5d4021945d4fd5
Tui Popenoe <tuipopenoe@gmail.com>
2014-07-06 00:14:12 -0500

    DrawGraphic.java updates

--- assignment2/GouraudRender3D.java
+++ assignment2/GouraudRender3D.java
@@ -1,8 +1,7 @@
 // Tui Popenoe
 // GouraudRender3D.java
 
-class PlaneEqn3
-{
+class PlaneEqn3{
     float dxdy, dzdy; 
     float y1, x1, z1;// ,x, y,  z;
     float drdy, dgdy,dbdy;
@@ -10,22 +9,20 @@ class PlaneEqn3
     public PlaneEqn3(int x1val, int y1val, int z1val
                         , float r1val,float g1val, float b1val
                         ,int x2val, int y2val, int z2val 
-                        , float r2val,float g2val, float b2val )
-    {
+                        , float r2val,float g2val, float b2val )    {
         x1=x1val; y1=y1val;z1=z1val; 
         r1=r1val;
         g1=g1val;
         b1=b1val;
-        if(y2val!=y1)
-        {
+        if(y2val!=y1)        {
            dxdy=(x2val-x1)/(y2val-y1);
            dzdy=(z2val-z1)/(y2val-y1);
            drdy=(r2val-r1)/(y2val-y1);
            dgdy=(g2val-g1)/(y2val-y1);
            dbdy=(b2val-b1)/(y2val-y1);
         }
-        else
-        {  dxdy=dzdy=drdy=dgdy=dbdy=0; }
+        else{  dxdy=dzdy=drdy=dgdy=dbdy=0; 
+        }
     }
     void next()
     {
@@ -40,29 +37,24 @@ class PlaneEqn3
     float getB(){return b1;}
 }
 
-public class GouraudRender3D extends Render3D
-{
-    protected void SortVertix(int  vertix_order[], int v[])
-    {
+public class GouraudRender3D extends Render3D{
+    protected void SortVertix(int  vertix_order[], int v[]){
         // sort vertix with y-coordinate
         int min,max,vmin,vmax;
-        if(vlist[v[0]].y>vlist[v[1]].y)
-        {
+        if(vlist[v[0]].y>vlist[v[1]].y){
              vmin=v[1];
              vmax=v[0];
              max=0;
              min=1;
         }
-        else
-        {
+        else{
              vmin=v[0];
              vmax=v[1];
              max=1;
              min=0;
         }
 
-        if(vlist[v[2]].y<=vlist[vmin].y)
-        {
+        if(vlist[v[2]].y<=vlist[vmin].y){
             v[0]=v[2];
             v[1]=vmin;
             v[2]=vmax;
@@ -70,10 +62,8 @@ public class GouraudRender3D extends Render3D
             vertix_order[1]=min;
             vertix_order[2]=max;
         }
-        else
-        {
-            if(vlist[v[2]].y>=vlist[vmax].y)
-            {
+        else{
+            if(vlist[v[2]].y>=vlist[vmax].y){
                 v[0]=vmin;
                 v[1]=vmax;
                 vertix_order[0]=min;
@@ -81,8 +71,7 @@ public class GouraudRender3D extends Render3D
                 vertix_order[2]=2;
 
             }
-            else
-            {
+            else{
                 v[0]=vmin;
                 v[1]=v[2];
                 v[2]=vmax;
@@ -93,8 +82,7 @@ public class GouraudRender3D extends Render3D
         }
     }
      public void Draw(Raster rast, int v[]
-                         ,float r[],float g[],float b[], int color)
-     {
+                         ,float r[],float g[],float b[], int color){
         int VertixOrder[]=new int[3];
         SortVertix(VertixOrder,  v);
          int x1=(int)vlist[v[0]].x
@@ -107,15 +95,20 @@ public class GouraudRender3D extends Render3D
            ,z2=((int)vlist[v[1]].z)>>11
            ,z3=((int)vlist[v[2]].z)>>11
            ;
-        if (y3<0 ||y1>rast.height )
+        if (y3<0 ||y1>rast.height ){
            return ;
-        if  ( ( x1<0&&x2<0&&x3<0) || ( x1>rast.width&&x2>rast.width&&x3>rast.width))
-           return ;
-       int area2=Area2(x1,y1,x2,y2,x3,y3);
-       if(Math.abs(area2)<1) return;
-       PlaneEqn3 p1,p2;
-       if(area2>0)                   
-       {
+        }
+
+        if  ( ( x1<0&&x2<0&&x3<0) || ( x1>rast.width&&x2>rast.width&&x3>rast.width)){
+            return ;
+        }
+
+        int area2=Area2(x1,y1,x2,y2,x3,y3);
+        if(Math.abs(area2)<1){
+            return;
+        }
+        PlaneEqn3 p1,p2;
+        if(area2>0){
             p1 =new PlaneEqn3(x1,y1,z1
                         ,r[VertixOrder[0]],g[VertixOrder[0]],b[VertixOrder[0]]
                         ,x3,y3,z3
@@ -124,9 +117,8 @@ public class GouraudRender3D extends Render3D
                         ,r[VertixOrder[0]],g[VertixOrder[0]],b[VertixOrder[0]]
                         ,x2,y2,z2
                         ,r[VertixOrder[1]],g[VertixOrder[1]],b[VertixOrder[1]]);
-         }
-         else
-         {
+        }
+        else{
             p2 =new  PlaneEqn3(x1,y1,z1
                         ,r[VertixOrder[0]],g[VertixOrder[0]],b[VertixOrder[0]]
                         ,x3,y3,z3
@@ -135,9 +127,8 @@ public class GouraudRender3D extends Render3D
                         ,r[VertixOrder[0]],g[VertixOrder[0]],b[VertixOrder[0]]
                         ,x2,y2,z2
                         ,r[VertixOrder[1]],g[VertixOrder[1]],b[VertixOrder[1]]);
-         }
-        for(int cy=y1;cy<y2;cy++)
-        {
+        }
+        for(int cy=y1;cy<y2;cy++){
             float tz=p1.getZ();
             float tr=p1.getR();
             float tg=p1.getG();
@@ -149,16 +140,11 @@ public class GouraudRender3D extends Render3D
             float drdx=(tr-p2.getR())/(tx1-tx2);
             float dgdx=(tg-p2.getG())/(tx1-tx2);
             float dbdx=(tb-p2.getB())/(tx1-tx2);
-            if(cy>=0&&cy<rast.height)
-            {
-
-                for( int cx=(int) tx1; cx<=tx2;cx++)
-                {
+            if(cy>=0&&cy<rast.height){
+                for( int cx=(int) tx1; cx<=tx2;cx++){
                     int offset=cy*rast.width;    
-                    if(cx>=0&& cx<rast.width)
-                    {
-                        if (tz <= rast.zbuff[offset+cx])
-                        {
+                    if(cx>=0&& cx<rast.width){
+                        if (tz <= rast.zbuff[offset+cx]){
                             if(tr>255f) tr=255;
                             if(tg>255f) tg=255;
                             if(tb>255f) tb=255;
@@ -179,16 +165,14 @@ public class GouraudRender3D extends Render3D
             }
             p1.next(); p2.next();
         }
-         if(area2>0)                   
-         {
+         if(area2>0){
               p2 =new PlaneEqn3(x2,y2,z2
                                 ,r[VertixOrder[1]],g[VertixOrder[1]],b[VertixOrder[1]]
                                 ,x3,y3,z3
                                 ,r[VertixOrder[2]],g[VertixOrder[2]],b[VertixOrder[2]]
                                 );
-         }
-         else
-         {
+        }
+        else{
               p1 =new PlaneEqn3(x2,y2,z2
                                 ,r[VertixOrder[1]],g[VertixOrder[1]],b[VertixOrder[1]]
                                 ,x3,y3,z3
@@ -197,8 +181,7 @@ public class GouraudRender3D extends Render3D
 
          }
  
-        for(int cy=y2;cy<y3;cy++)
-        {
+        for(int cy=y2;cy<y3;cy++){
             float tz=p1.getZ();
             float tr=p1.getR();
             float tg=p1.getG();
@@ -211,15 +194,11 @@ public class GouraudRender3D extends Render3D
             float drdx=(tr-p2.getR())/(tx1-tx2);
             float dgdx=(tg-p2.getG())/(tx1-tx2);
             float dbdx=(tb-p2.getB())/(tx1-tx2);
-            if(cy>=0&&cy<rast.height)
-            {
+            if(cy>=0&&cy<rast.height){
                 int offset=cy*rast.width;    
-                for( int cx=(int) tx1; cx<=tx2;cx++)
-                {
-                    if(cx>=0&& cx<rast.width)
-                    {
-                        if (tz <= rast.zbuff[offset+cx]) 
-                        {
+                for( int cx=(int) tx1; cx<=tx2;cx++){
+                    if(cx>=0&& cx<rast.width){
+                        if (tz <= rast.zbuff[offset+cx]){
                             if(tr>255f) tr=255;
                             if(tg>255f) tg=255;
                             if(tb>255f) tb=255;
@@ -240,8 +219,7 @@ public class GouraudRender3D extends Render3D
             p1.next(); p2.next();
         }
     }
-
- }
+}
 
 
 
--- assignment2/TRenderer.js
+++ assignment2/TRenderer.js
@@ -1,6 +1,9 @@
+// Tui Popenoe
+// TRenderer.js
+
 function changeSize(pic){
     document.images[pic].width = 45;
-    document.images[pic].height = 53;
+    document.images[pic].height = 48;
 }
 
 function resetSize(pic){
@@ -13,80 +16,80 @@ function update(){
 }
 
 function resetVal(){
-    document.GraphicForm.reflectance.checked = true;
-    document.GraphicForm.directionalLight.checked = true;
-    document.GraphicForm.pointLight.checked = true;
-    document.GraphicForm.ambientLight.checked = true;
-    document.GraphicForm.Angle.value = 0;
-    document.GraphicForm.RotateX1.value = 0;
-    document.GraphicForm.RotateX2.value = 0;
-    document.GraphicForm.RotateY1.value = 0;
-    document.GraphicForm.RotateY2.value = 0;
-    document.GraphicForm.RotateZ1.value = 0;
-    document.GraphicForm.RotateZ2.value = 0;
-    document.GraphicForm.TranslateX.value = 0;
-    document.GraphicForm.TranslateY.value = 0;
-    document.GraphicForm.TranslateZ.value = 0;
-    document.GraphicForm.ScaleX.value = 0;
-    document.GraphicForm.ScaleY.value = 0;
-    document.GraphicForm.ScaleZ.value = 0;
+    document.getElementById(reflectance).checked = true;
+    document.getElementById(directionalLight).checked = true;
+    document.getElementById(pointLight).checked = true;
+    document.getElementById(ambientLight).checked = true;
+    document.getElementById(angle).value = 0;
+    document.getElementById(rotateX1).value = 0;
+    document.getElementById(rotateX2).value = 0;
+    document.getElementById(rotateY1).value = 0;
+    document.getElementById(rotateY2).value = 0;
+    document.getElementById(rotateZ1).value = 1;
+    document.getElementById(rotateZ2).value = -1;
+    document.getElementById(translateX).value = 0;
+    document.getElementById(translateY).value = 0;
+    document.getElementById(translateZ).value = 0;
+    document.getElementById(scaleX).value = 0;
+    document.getElementById(scaleY).value = 0;
+    document.getElementById(scaleZ).value = 0;
 
     document.DrawGraphic.UPDATE();
 }
 
 function get3DObject(model){
-    document.GraphicForm.displayObject(model);
-    document.GraphicForm.shape[1].checked = true;
+    document.DrawGraphic.displayObject(model);
+    document.shape1.checked = true;
 }
 
 function passVal(){
-    document.DrawGraphic.angleVal(document.GraphicForm.Angle.value);
-    document.DrawGraphic.rotateX1Val(document.GraphicForm.RotateX1.value);
-    document.DrawGraphic.rotateX2Val(document.GraphicForm.RotateX2.value);
-    document.DrawGraphic.rotateY1Val(document.GraphicForm.RotateY1.value);
-    document.DrawGraphic.rotateY2Val(document.GraphicForm.RotateY2.value);
-    document.DrawGraphic.rotateZ1Val(document.GraphicForm.RotateZ1.value);
-    document.DrawGraphic.rotateZ2Val(document.GraphicForm.RotateZ2.value);
-    document.DrawGraphic.translateXVal(document.GraphicForm.TranslateX.value);
-    document.DrawGraphic.translateYVal(document.GraphicForm.TranslateY.value);
-    document.DrawGraphic.translateZVal(document.GraphicForm.TranslateZ.value);
-    document.DrawGraphic.scaleXVal(document.GraphicForm.ScaleX.value);
-    document.DrawGraphic.scaleYVal(document.GraphicForm.ScaleY.value);
-    document.DrawGraphic.scaleZVal(document.GraphicForm.ScaleZ.value);
+    document.DrawGraphic.angleVal(document.angle.value);
+    document.DrawGraphic.rotateX1Val(document.getElementById(rotateX1.value));
+    document.DrawGraphic.rotateX2Val(document.getElementById(rotateX2.value));
+    document.DrawGraphic.rotateY1Val(document.getElementById(rotateY1.value));
+    document.DrawGraphic.rotateY2Val(document.getElementById(rotateY2.value));
+    document.DrawGraphic.rotateZ1Val(document.getElementById(rotateZ1.value));
+    document.DrawGraphic.rotateZ2Val(document.getElementById(rotateZ2.value));
+    document.DrawGraphic.translateXVal(document.getElementById(translateX.value));
+    document.DrawGraphic.translateYVal(document.getElementById(translateY.value));
+    document.DrawGraphic.translateZVal(document.getElementById(translateZ.value));
+    document.DrawGraphic.scaleXVal(document.getElementById(scaleX.value));
+    document.DrawGraphic.scaleYVal(document.getElementById(scaleY.value));
+    document.DrawGraphic.scaleZVal(document.getElementById(scaleZ.value));
 
     document.DrawGraphic.UPDATE();
 }
 
 function reflectancePassVal(){
-    document.DrawGraphic.ReflectanceVal(document.GraphicForm.reflectance.checked);
+    document.DrawGraphic.ReflectanceVal(document.getElementById(reflectance).checked);
     document.DrawGraphic.UPDATE();
 }
 
 function directionalLightPassVal(){
-    document.DrawGraphic.DirectionalLightVal(document.GraphicForm.directionalLight.checked);
+    document.DrawGraphic.DirectionalLightVal(document.getElementById(directionalLight).checked);
     document.DrawGraphic.UPDATE();
 }
 
 function pointLightPassVal(){
-    document.DrawGraphic.PointLightVal(document.GraphicForm.pointLight.checked);
+    document.DrawGraphic.PointLightVal(document.getElementById(pointLight).checked);
     document.DrawGraphic.UPDATE();
 }
 
 function ambientLightPassVal(){
-    document.DrawGraphic.AmbientLightVal(document.GraphicForm.ambientLight.checked);
+    document.DrawGraphic.AmbientLightVal(document.getElementById(ambientLight).checked);
     document.DrawGraphic.UPDATE();
 }
 
 function GouraudShapePassVal(){
-    if(document.GraphicForm.shape[1].checked==true){
-        document.DrawGraphic.GouraudShapeVal(document.GraphicForm.shape[1].checked);
+    if(document.shapeRadio2.checked==true){
+        document.DrawGraphic.GouraudShapeVal(document.getElementById(shapeRadio2).checked);
         document.DrawGraphic.UPDATE();
     }
 }
 
 function FlatShapePassVal(){
-    if(document.GraphicForm.shape[0].checked==true){
-        document.DrawGraphic.FlatShapeVal(document.GraphicForm.shape[0].checked);
+    if(document.shapeRadio2.checked==true){
+        document.DrawGraphic.FlatShapeVal(document.getElementById(shapeRadio1).checked);
         document.DrawGraphic.UPDATE();
     }
 }
