bef0b2a5a2438228ba45f6398ebee77fde49755c
Tui Popenoe <tuipopenoe@gmail.com>
2014-07-09 20:12:58 -0500

    Working applet

--- assignment2/Drawable.java
+++
@@ -1,8 +0,0 @@
-// Tui Popenoe
-// Drawable.java
-
-// Abstract Drawable class
-public abstract interface Drawable {
-    public abstract void Draw(Raster r);
-    public abstract void Illuminate(Light l[], int lights);
-}
