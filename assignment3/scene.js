583b252c4610f1d854d471827862ef0ffa364c25
Tui Popenoe <tuipopenoe@gmail.com>
2014-07-20 19:35:35 -0500

    Assignment 3 updates

--- assignment3/scene.js
+++ assignment3/scene.js
@@ -1,9 +1,16 @@
 var canvas;
 var engine;
-var currentScene;
-var currentObject;
+var camera;
+var light;
+var scene;
+var object;
+var material;
+
+var previousDate = Date.now();
+var now;
 var currentFPS;
 var divCurrentFPS;
+var renderContent;
 
 window.requestAnimationFrame = (function(){
     return window.requestAnimationFrame ||
@@ -15,66 +22,123 @@ window.requestAnimationFrame = (function(){
     })();
 
 window.onload = function(){
-    loadScene(createScene());
+    createScene();
 }
 
 function init(){
     canvas = document.getElementById('renderCanvas');
+    renderContent = document.getElementById('renderContent');
     engine = new BABYLON.Engine(canvas, true);
     divCurrentFPS = document.getElementById('currentFPS');
+    if(typeof object != 'undefined'){
+            object.rotation.y = 0;
+        object.rotation.x = 0;
+    }
 }
 
 var createScene = function(){
-    var scene = new BABYLON.Scene(engine);
+    if(BABYLON.Engine.isSupported()){
+        init();
+    }
+    scene = new BABYLON.Scene(engine);
 
-    var camera = new BABYLON.ArcRotateCamera('Camera', 0, Math.PI / 2, 12, 
+    camera = new BABYLON.ArcRotateCamera('Camera', 0, Math.PI / 2, 12, 
         BABYLON.Vector3.Zero(), scene);
 
-    var light = new BABYLON.HemisphericLight('hemi', 
+    light = new BABYLON.HemisphericLight('hemi', 
         new BABYLON.Vector3(0, 1, 0), scene);
 
-    var object = randomizeObject()
-    //BABYLON.Mesh.CreateTorusKnot('mesh', 2, 0.5, 128, 64, 2, 3, 
-    //    scene);
+    object = randomizeObject()
+        /*BABYLON.Mesh.CreateTorusKnot('mesh', 2, 0.5, 128, 64, 2, 3, 
+           scene);*/
 
-    var material = new BABYLON.StandardMaterial('std', scene);
-    material.diffuseColor = new BABYLON.Color3(0.5, 0, 0.5);
+    material = new BABYLON.StandardMaterial('std', scene);
+    material.diffuseColor = randomColor(); //new BABYLON.Color3(0.5, 0, 0.5);
+    material.backFaceCulling = false;
 
     object.material = material;
 
-    return scene;
+    engine.runRenderLoop(function(){
+        object.rotation.y += 0.002;
+        object.rotation.x += 0.002;
+        scene.render();
+        previousDate = Date.now();
+    }, calcFPS());
+    //return scene;
 }
 
-var randomizeObject = function(){
-    num = Math.floor(Math.random() *(7-1)) + 1;
+var rSize = function(min, max, mod){
+    return Math.floor(Math.random() * (max - min)) + 1 + mod;
+}
 
-    switch(num):
+// Return Random Color
+var randomColor = function(){
+    return new BABYLON.Color3(Math.random(), Math.random(),
+        Math.random(), Math.random());
+}
+var randomizeObject = function(){
+    num = Math.floor(Math.random() *(8-1)) + 1;
+
+    switch(num){
         case 1:
-            return BABYLON.Mesh.CreateBox('mesh', 6.0, scene);
+            renderContent.textContent = 'Box';
+            return BABYLON.Mesh.CreateBox('mesh', rSize(2, 6, 3), scene);
             break;
         case 2:
-            return BABYLON.Mesh.CreateSphere('mesh', 10.0, 3.0, scene);
+            renderContent.textContent = 'Sphere';
+            return BABYLON.Mesh.CreateSphere('mesh', rSize(20, 30, 3), 
+                rSize(1, 3, 2), scene);
             break;
         case 3:
-            return BABYLON.Mesh.CreatePlane('mesh', 15.0, scene);
+            renderContent.textContent = 'Plane';
+            return BABYLON.Mesh.CreatePlane('mesh', rSize(5, 13, 1), scene);
             break;
         case 4:
-            return BABYLON.Mesh.CreateCylinder('mesh', 3, 3, 3, 6, scene, 
+            renderContent.textContent = 'Cylinder';
+            var i = rSize(6, 8, 0);
+            return BABYLON.Mesh.CreateCylinder('mesh', 
+                rSize(5, 10, 1),
+                i,
+                i,
+                rSize(12, 40, 0), scene, 
                 false);
             break;
         case 5:
-            return BABYLON.Mesh.CreateTorus('mesh', 5, 1, 10, scene, false);
+            renderContent.textContent = 'Small Torus';
+            return BABYLON.Mesh.CreateTorus('mesh', 
+                rSize(5, 10, 0),
+                rSize(1, 3, 0),
+                rSize(10, 20, 0),
+                 scene, false);
             break;
         case 6: 
-            return BABYLON.Mesh.CreateGround('mesh', 10.0, 10.0, 10, scene,
+            renderContent.textContent = 'Large Torus';
+            return BABYLON.Mesh.CreateTorus('mesh', 
+                rSize(8, 12, 0), 
+                rSize(1.5, 3, 0),
+                rSize(20, 40, 0), scene,
                 false);
             break;
         case 7:
-            return BABYLON.Mesh.CreateTorusKnow('mesh', 2, 0.5, 128, 64, 2, 3,
+            renderContent.textContent = 'Torus Knot';
+            return BABYLON.Mesh.CreateTorusKnot('mesh', 2, 0.5, 128, 64, 2, 3,
                 scene);
             break;
         default:
-         return;
-         break;
+            renderContent.textContent = 'Default Torus'
+            return BABYLON.Mesh.CreateTorus('mesh', 
+                rSize(5, 6, 0),
+                rSize(1, 2, 0),
+                rSize(10, 16, 0),
+                 scene, false);
+            break;
+    }
+}
 
+var calcFPS = function(){
+    var now = Date.now();
+    var currentFPS = 1000 / (now - previousDate);
+    previousDate = now;
+
+    divCurrentFPS.textContent = currentFPS.toFixed(2);
 }
